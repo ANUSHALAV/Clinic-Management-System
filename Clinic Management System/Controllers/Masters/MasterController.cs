5224d43e4151fb7e8fcd5385fa6db42bc3603cdb
ANUSHALAV <bhattanushalav@gmail.com>
2026-03-10 23:16:59 +0530

    Add master controller, country model, and MongoDB packages
    Added MasterController with Index action and CountryMaster model. Introduced empty MasterServices class and IMasterServices interface. Updated project file to include MongoDB.Bson and MongoDB.Driver.Core packages, and adjusted folder includes.

---
+++ Clinic Management System/Controllers/Masters/MasterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic_Management_System.Controllers.Masters
+{
+    public class MasterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Clinic Management System/Models/Masters/CountryMaster.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinic_Management_System.Models.Masters
+{
+    public class CountryMaster
+    {
+        public string CountryId { get; set; }
+        public string CountryName { get; set; }
+        public int Status { get; set; }
+    }
+}
